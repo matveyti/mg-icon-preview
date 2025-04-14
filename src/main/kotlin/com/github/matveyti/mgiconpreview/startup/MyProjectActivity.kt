19d61a5a79e90794551912e21f35e0313276973c
GitHub Action <action@github.com>
2025-04-14 12:47:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/matveyti/mgiconpreview/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/matveyti/mgiconpreview/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.matveyti.mgiconpreview.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
